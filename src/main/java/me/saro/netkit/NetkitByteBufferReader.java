3aff71a09fc39b02aa5bf7949768fca65ec8a8dc
PARK Yong Seo <j@saro.me>
2018-10-29 23:11:36 +0900

    test

--- src/main/java/me/saro/netkit/NetkitByteBufferReader.java
+++ src/main/java/me/saro/netkit/NetkitByteBufferReader.java
@@ -14,16 +14,16 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 public class NetkitByteBufferReader {
     public void accept() {
-        asynchronousServerSocketChannel.accept(null, new CompletionHandler<AsynchronousSocketChannel, Void>() {
-            @SneakyThrows
-            public void completed(final AsynchronousSocketChannel channel, Void v) {
-                if (!asynchronousServerSocketChannel.isOpen()) {
-                    return;
-                }
-            }
-            public void failed(Throwable error, Void v) {
-                throw new RuntimeException(error);
-            }
-        });
+//        asynchronousServerSocketChannel.accept(null, new CompletionHandler<AsynchronousSocketChannel, Void>() {
+//            @SneakyThrows
+//            public void completed(final AsynchronousSocketChannel channel, Void v) {
+//                if (!asynchronousServerSocketChannel.isOpen()) {
+//                    return;
+//                }
+//            }
+//            public void failed(Throwable error, Void v) {
+//                throw new RuntimeException(error);
+//            }
+//        });
     }
 }
