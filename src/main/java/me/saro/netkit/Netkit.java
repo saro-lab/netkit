89f05bf21a870315638ab3a1d47d26074c10507d
PARK Yong Seo <j@saro.me>
2018-11-07 18:08:14 +0900

    TEMP CODE

--- src/main/java/me/saro/netkit/Netkit.java
+++ src/main/java/me/saro/netkit/Netkit.java
@@ -1,6 +1,7 @@
 package me.saro.netkit;
 
 import java.nio.ByteBuffer;
+import java.util.Arrays;
 
 /**
  * Netkit
@@ -29,4 +30,22 @@ public class Netkit {
         buffer.compact();
         return ba;
     }
+    
+    @Deprecated // NEED TEST
+    public static byte[] safeCopyArray(ByteBuffer buffer, byte[] bytes) {
+        return safeCopyArray(buffer, bytes, 0, buffer.remaining());
+    }
+    
+    @Deprecated // NEED TEST
+    public static byte[] safeCopyArray(ByteBuffer buffer, byte[] bytes, int offset, int length) {
+        if (buffer.hasArray()) {
+            return Arrays.copyOf(buffer.array(), length);
+        }
+        
+        buffer.get(bytes, offset, length);
+        buffer.compact();
+        return bytes;
+    }
+    
+    
 }
--- src/main/java/me/saro/netkit/NetkitServerAccepter.java
+++ src/main/java/me/saro/netkit/NetkitServerAccepter.java
@@ -1,6 +1,8 @@
 package me.saro.netkit;
 
 import java.nio.channels.AsynchronousServerSocketChannel;
+import java.nio.channels.AsynchronousSocketChannel;
+import java.nio.channels.CompletionHandler;
 import java.util.Map;
 import java.util.function.Function;
 
@@ -9,6 +11,7 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.log4j.Log4j2;
 import me.saro.commons.function.ThrowableConsumer;
+import me.saro.commons.function.ThrowablePredicate;
 
 /**
  * Netkit Server Accepter
@@ -23,20 +26,51 @@ public class NetkitServerAccepter {
     @Getter @Setter(value=AccessLevel.PACKAGE) int byteBufferUnitSize;
     @Getter @Setter(value=AccessLevel.PACKAGE) AsynchronousServerSocketChannel asynchronousServerSocketChannel;
     @Getter @Setter(value=AccessLevel.PACKAGE) Map<String, NetkitConnection> connections;
-
-    public NetkitServerAccepter prev(Function<String, String> prev) {
-        return this;
-    }
     
-    public NetkitServerAccepter next(Function<String, String> prev) {
+    ThrowableConsumer<Throwable> throwableConsumer;
+    ThrowablePredicate<AsynchronousSocketChannel> filter;
+
+    public NetkitServerAccepter filter(ThrowablePredicate<AsynchronousSocketChannel> filter) {
+        this.filter = filter;
         return this;
     }
     
     public NetkitServerAccepter error(ThrowableConsumer<Throwable> throwableConsumer) {
+        this.throwableConsumer = throwableConsumer;
         return this;
     }
     
     public <T extends NetkitReader> T read(T t) {
+        
+        asynchronousServerSocketChannel.accept(null, new CompletionHandler<AsynchronousSocketChannel, Void>() {
+            
+            @Override public void completed(AsynchronousSocketChannel channel, Void v) {
+                try {
+                    filter
+                } catch (Exception e) {
+                    try {
+                        channel.close();
+                    } catch (Exception e2)
+                }
+            }
+
+            @Override public void failed(Throwable throwable, Void v) {
+                error(throwable);
+            }
+        });
+        
         return t;
+    }
+    
+    private void error(Throwable throwable) {
+        try {
+            if (throwableConsumer != null) {
+                throwableConsumer.accept(throwable);
+            } else {
+                log.error(throwable);
+            }
+        } catch (Exception e) {
+            log.error(e);
+        }
     }
 }
